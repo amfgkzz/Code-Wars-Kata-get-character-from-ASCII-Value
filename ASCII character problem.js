360d53913e20fcd32b0acc556df694d9531252e1
Koua Xiong <kxiong517@gmail.com>
2022-01-19 20:25:21 -0600

    coercion

--- ASCII character problem.js
+++ ASCII character problem.js
@@ -36,6 +36,7 @@ function getChar(number) {
     let char;
 
     for (const num in ASCII) {
+        // use double equal for coercion
         if ( num == number ) {
             const char = ASCII[num];
             return char;
