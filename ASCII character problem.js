f019f29d813067500e354214bbf4afcafc53e23c
Koua Xiong <kxiong517@gmail.com>
2022-01-19 20:21:09 -0600

    explanation

--- ASCII character problem.js
+++ ASCII character problem.js
@@ -1,6 +1,10 @@
 // Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
 
 // first what is an ASCII character?
+// From Wikipedia:
+// ASCII, abbreviated from American Standard Code for Information Interchange, 
+// is a character encoding standard for electronic communication. ASCII codes represent text in computers, telecommunications equipment, 
+// and other devices. Most modern character-encoding schemes are based on ASCII, although they support many additional characters. 
 
 function getChar(number) {
 
