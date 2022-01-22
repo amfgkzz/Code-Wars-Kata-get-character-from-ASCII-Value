ddd4fd6cf18bfd7caf49e056342a6077713be47a
Koua Xiong <kxiong517@gmail.com>
2022-01-22 14:18:39 -0600

    completed revised

--- Gravity-Flip/src/flip.js
+++ Gravity-Flip/src/flip.js
@@ -1,8 +1,8 @@
 module.exports = function flip(d, array) {
     switch (d) {
         case 'R':
-            return rightSide = array.sort(function(a, b){return a-b})
+            return array.sort(function(a, b){return a-b})
         case 'L':
-            return leftSide = array.sort(function(a, b){return b-a})
+            return array.sort(function(a, b){return b-a})
     }
 }
