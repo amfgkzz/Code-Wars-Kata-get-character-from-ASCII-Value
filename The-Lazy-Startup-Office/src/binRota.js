d05c69835c63b6dfaddb1a441b8ca0070dbb0235
Koua Xiong <kxiong517@gmail.com>
2022-01-25 19:05:06 -0600

    created function with if else

--- The-Lazy-Startup-Office/src/binRota.js
+++ The-Lazy-Startup-Office/src/binRota.js
@@ -1,10 +1,25 @@
-module.exports = function binRota(a){
-    let newArray;
+module.exports = function binRota(array){
+    let newArray = [];
 
-    for (let i = 0; i < a.length; i++) {
-        let element = a[i];
-        
+    for (let i = 0; i < array.length; i++) {
+        let innerArray = array[i];
+        if (i % 2 !== 0) {
+            for (let k = innerArray.length - 1; k >= 0; k--) {
+                let elementReverse = innerArray[k];
+                newArray.push(elementReverse);
+            }
+        } else {
+            for (let j = 0; j < innerArray.length; j++) {
+                let element = innerArray[j];
+                newArray.push(element);
+            }
+        }
     }
 
+    console.log(newArray);
     return newArray;
 }
+
+// loop through the array
+// each inner array is added to new array
+// index of i % 2 != 0, is put into the new array in reversed order
