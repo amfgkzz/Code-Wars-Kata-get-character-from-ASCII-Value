0f690b6a3772f97e34e432adfaed14220ac583f1
Koua Xiong <kxiong517@gmail.com>
2022-01-25 19:13:53 -0600

    added another test

--- The-Lazy-Startup-Office/src/binRota.js
+++ The-Lazy-Startup-Office/src/binRota.js
@@ -16,7 +16,6 @@ module.exports = function binRota(array){
         }
     }
 
-    console.log(newArray);
     return newArray;
 }
 
--- The-Lazy-Startup-Office/src/binRota.test.js
+++ The-Lazy-Startup-Office/src/binRota.test.js
@@ -3,7 +3,13 @@ let binRota = require('./binRota');
 
 describe("Test for array", function(){
     let a = [];
+    let b = [ ["Stefan", "Raj",    "Marie"],
+    ["Alexa",  "Amy",    "Edward"],
+    ["Liz",    "Claire", "Juan"],
+    ["Dee",    "Luke",   "Katie"] ];
+
     it("", function(){
         assert.typeOf(binRota(a), 'array');
+        assert.deepEqual(binRota(b), ["Stefan", "Raj", "Marie", "Edward", "Amy", "Alexa", "Liz", "Claire", "Juan", "Katie", "Luke", "Dee"]);
     })
 })
