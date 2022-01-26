b66d7b249445fb8b869b24af00a9e361be86c7cb
Koua Xiong <kxiong517@gmail.com>
2022-01-26 15:52:26 -0600

    revised function completed

--- Credit-Card-Mask/src/maskify.js
+++ Credit-Card-Mask/src/maskify.js
@@ -1,13 +1,3 @@
 module.exports = function maskify(string) {
-    return string.length > 4 
-    ?
-    hash(string)
-    : 
-    string
-}
-
-function hash(string){
-    let hash = string.slice(0, -4).replace(/./g, '#');
-    let keepText = string.slice(string.length - 4);
     return string.slice(0, -4).replace(/./g, '#') + string.slice(string.length - 4);
 }
