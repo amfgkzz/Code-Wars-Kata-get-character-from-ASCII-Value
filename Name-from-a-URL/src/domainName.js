af4aff4e764b18da2a5994859357c8ee14376a34
Koua Xiong <kxiong517@gmail.com>
2022-01-26 18:47:26 -0600

    passed 2nd test

--- Name-from-a-URL/src/domainName.js
+++ Name-from-a-URL/src/domainName.js
@@ -1,6 +1,25 @@
 module.exports = function domainName(url){
     let parsedUrl = '';
+    let parsedUrlTwo = '';
+    let dotCount = 0;
 
-    
-    return parsedUrl;
+    for (let i = 0; i < url.length; i++) {
+        let element = url[i];
+        if ( parsedUrl == 'http://' || parsedUrl == 'https://' ) {
+            parsedUrl = ''; // reset
+        }
+
+        if ( element == '.' && dotCount > 0 ) {
+            return parsedUrl;
+        }
+
+        if ( element == '.' && dotCount == 0 ) {
+            dotCount++; // count the dots
+            parsedUrlTwo = parsedUrl; // store the letters before the first dot
+            parsedUrl = ''; // reset
+        }
+        parsedUrl += element;
+    }
+
+    return parsedUrlTwo;
 }
