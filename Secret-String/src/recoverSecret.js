855f4fc1f672c4f53827c6df368148ecc10e24c6
Koua Xiong <kxiong517@gmail.com>
2022-01-27 01:45:24 -0600

    better functionality and clearer thought process

--- Secret-String/src/recoverSecret.js
+++ Secret-String/src/recoverSecret.js
@@ -1,16 +1,29 @@
 module.exports = 
 function recoverSecret(triplet){
     let secretWord = '';
-    let secretValue = {};
+    let secretValue = new Object();
+    let count = 0;
 
-    const compare = function(a){
+    for (let i = 0; i < triplet.length; i++) {
+        let randomTriplet = triplet[i];
+        for (let j = 0; j < randomTriplet.length; j++) {
+            const letter = randomTriplet[j];
+            if (!secretValue[letter]) {
+                secretValue[letter] = count;
+                count++;
+            }
 
+
+        }
     }
 
-    for (let i = 1; i < triplet.length; i++) {
-        const compareArray = triplet[0];
-        const element = triplet[i];
-        
-        element.map(compare);
-    }
+    // console.log('secretWord: ', secretWord)
+    console.log('secretValue: ', secretValue)
 }
+
+// Latest thoughts b4 sleep:
+// hatsupwi, tis, hatupwis, whatupis, order matters function could return whatupis instead of whatisup depending on 
+// how the algorithm moves the word positions
+// could use a while loop, I could see it making the logic easier
+// logic may be easier with using arrays n using array methods
+// keeping position values may not be needed, either way would work tho
--- Secret-String/src/recoverSecret.test.js
+++ Secret-String/src/recoverSecret.test.js
@@ -12,10 +12,22 @@ describe('Recover Secret Test', function(){
         ['w','h','s']
         ] 
     it('Type test', function(){
-
+        assert.typeOf(recoverSecret(triplets1), 'string')
     })
 
     it('Equal test', function(){
         
     })
 })
+
+// tup, whi
+// tupwhi, tsu
+// tsupwhi, ats
+// atsupwhi, hap
+// hatsupwi, tis, hatupwis, whatupis, order matters function could return whatupis instead of whatisup depending on 
+// how the algorithm moves the word positions
+// hatisupw, whs
+// whatisup
+
+// 012, 345
+// 012345, 678
