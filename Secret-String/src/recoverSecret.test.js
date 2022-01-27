dcd389ed661553a0c414e2c8303f071817205a4c
Koua Xiong <kxiong517@gmail.com>
2022-01-26 20:56:31 -0600

    Secret string task

---
+++ Secret-String/src/recoverSecret.test.js
@@ -0,0 +1,12 @@
+let assert = require('chai').assert;
+let recoverSecret = require('./recoverSecret');
+
+describe('Recover Secret Test', function(){
+    it('Type test', function(){
+
+    })
+
+    it('Equal test', function(){
+        
+    })
+})
