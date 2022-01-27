17d325e9005b50203e56aa9aee6001ac9f5b593c
Koua Xiong <kxiong517@gmail.com>
2022-01-26 18:49:55 -0600

    second domain name test case

--- Name-from-a-URL/src/domainName.test.js
+++ Name-from-a-URL/src/domainName.test.js
@@ -3,6 +3,7 @@ let domainName = require('./domainName');
 
 describe('Domain Name Test', function(){
     let urlOne = "http://github.com/carbonfive/raygun";
+    let urlTwo = "http://www.zombie-bites.com";
 
     it('Type test', function(){
         assert.typeOf(domainName(urlOne), 'string')
@@ -10,5 +11,6 @@ describe('Domain Name Test', function(){
 
     it('Parsed domain name Test', function(){
         assert.equal(domainName(urlOne), 'github')
+        assert.equal(domainName(urlTwo), 'zombie-bites')
     })
 })
