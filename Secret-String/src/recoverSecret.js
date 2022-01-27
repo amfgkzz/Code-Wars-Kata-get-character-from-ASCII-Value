58da584abab601934933f0649ca0fd1c69305fa7
Koua Xiong <kxiong517@gmail.com>
2022-01-26 22:40:26 -0600

    thought process and beginning functionality

--- Secret-String/src/recoverSecret.js
+++ Secret-String/src/recoverSecret.js
@@ -1,3 +1,16 @@
-module.exports = function recoverSecret(triplet){
-    
+module.exports = 
+function recoverSecret(triplet){
+    let secretWord = '';
+    let secretValue = {};
+
+    const compare = function(a){
+
+    }
+
+    for (let i = 1; i < triplet.length; i++) {
+        const compareArray = triplet[0];
+        const element = triplet[i];
+        
+        element.map(compare);
+    }
 }
--- Secret-String/src/recoverSecret.test.js
+++ Secret-String/src/recoverSecret.test.js
@@ -2,6 +2,15 @@ let assert = require('chai').assert;
 let recoverSecret = require('./recoverSecret');
 
 describe('Recover Secret Test', function(){
+    let triplets1 = [
+        ['t','u','p'],
+        ['w','h','i'],
+        ['t','s','u'],
+        ['a','t','s'],
+        ['h','a','p'],
+        ['t','i','s'],
+        ['w','h','s']
+        ] 
     it('Type test', function(){
 
     })
