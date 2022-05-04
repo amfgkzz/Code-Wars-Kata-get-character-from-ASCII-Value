91899a5455a7716a15f4c4a3b97420e37524e00f
Koua Xiong <kxiong517@gmail.com>
2022-05-03 22:02:14 -0500

    for loop for if statements

--- Binary Addition/src/binaryAdd.js
+++ Binary Addition/src/binaryAdd.js
@@ -1,25 +1,17 @@
 module.exports = function binaryAdd(num1, num2){
     let addedNum = num1 + num2;
+    let binaryCheck = [256, 128, 64, 32, 16, 8, 4, 2];
+    let finalBinaryNum = [];
 
-    // 256 128 64 32 16 8 4 2 1
-    if (addedNum >= 256) {
-        
-    } else if (addedNum >= 128) {
-
-    } else if (addedNum >= 64) {
-
-    } else if (addedNum >= 32) {
-        
-    } else if (addedNum >= 16) {
-        
-    } else if (addedNum >= 8) {
-        
-    } else if (addedNum >= 4) {
-        
-    } else if (addedNum >= 2) {
-        
+    for (let i = 0; i < binaryCheck.length; i++) {
+        const binaryNum = array[i];
+        if (addedNum >= binaryNum) {
+            addedNum -= binaryNum;
+            finalBinaryNum.push(1);
+        } else {
+            finalBinaryNum.push(0);
+        }
     }
 
-    // 100100010
     return
 }
