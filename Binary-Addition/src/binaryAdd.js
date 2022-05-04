9622e5178b46189de0604d9feb6790c64afd8837
Koua Xiong <kxiong517@gmail.com>
2022-05-04 11:43:16 -0500

    completed all test with new big number test

--- Binary-Addition/src/binaryAdd.js
+++ Binary-Addition/src/binaryAdd.js
@@ -1,23 +1,16 @@
 module.exports = function binaryAdd(num1, num2){
     let addedNum = num1 + num2;
-    let binaryCheck = [256, 128, 64, 32, 16, 8, 4, 2, 1];
     let finalBinaryNum = [];
 
-    for (let i = 0; i < binaryCheck.length; i++) {
-        const binaryNum = binaryCheck[i];
-        if (addedNum >= binaryNum) {
-            addedNum -= binaryNum;
-            finalBinaryNum.push(1);
-        } else if (finalBinaryNum.length > 0) {
-            finalBinaryNum.push(0);
+    while (addedNum >= 1) {
+        if (addedNum % 2) {
+            finalBinaryNum.unshift(1);
+            addedNum = parseInt(addedNum / 2);
+            console.log('addedNum: ', addedNum);
+        } else {
+            finalBinaryNum.unshift(0);
+            addedNum /= 2;
         }
     }
-
-    // 010001001
-    // will need to rework the function for big numbers,
-    // function should divide addedNum by 2, check if remainder is 1 or 0,
-    // if 1, add 1 to finalBinaryNum, parseInt addedNum then divide addedNum by 2
-    // if 0, add 0 to finalBinaryNum, divide addedNum by 2
-    // keep going until parseInt = 0? test this use https://byjus.com/maths/binary-number-system/ to help
     return finalBinaryNum.join('');
 }
--- Binary-Addition/src/binaryAdd.test.js
+++ Binary-Addition/src/binaryAdd.test.js
@@ -8,5 +8,6 @@ describe('binary Add test', function(){
     it('final binary number test', function(){
         assert.equal(binaryAdd(1,2), '11');
         assert.equal(binaryAdd(150,140), '100100010');
+        assert.equal(binaryAdd(759092589957578,975933433650968), '110001010011111111100111001110100000011000011100010');
     })
 });
