e8d35c53a4231f30291fb531cd0aba3da58f030c
Koua Xiong <kxiong517@gmail.com>
2022-05-03 21:39:49 -0500

    if else checks

---
+++ Binary Addition/src/binaryAdd.js
@@ -0,0 +1,25 @@
+module.exports = function binaryAdd(num1, num2){
+    let addedNum = num1 + num2;
+
+    // 256 128 64 32 16 8 4 2 1
+    if (addedNum >= 256) {
+        
+    } else if (addedNum >= 128) {
+
+    } else if (addedNum >= 64) {
+
+    } else if (addedNum >= 32) {
+        
+    } else if (addedNum >= 16) {
+        
+    } else if (addedNum >= 8) {
+        
+    } else if (addedNum >= 4) {
+        
+    } else if (addedNum >= 2) {
+        
+    }
+
+    // 100100010
+    return
+}
