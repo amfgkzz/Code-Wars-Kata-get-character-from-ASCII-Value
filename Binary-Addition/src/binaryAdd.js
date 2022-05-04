cf1c9a8c14ca1328aa2c3617edd0a1c99d554f27
Koua Xiong <kxiong517@gmail.com>
2022-05-03 23:37:07 -0500

    flawed function for big numbers, needs rework

--- Binary-Addition/src/binaryAdd.js
+++ Binary-Addition/src/binaryAdd.js
@@ -13,6 +13,11 @@ module.exports = function binaryAdd(num1, num2){
         }
     }
 
-    console.log('actual: ', finalBinaryNum.join(''))
+    // 010001001
+    // will need to rework the function for big numbers,
+    // function should divide addedNum by 2, check if remainder is 1 or 0,
+    // if 1, add 1 to finalBinaryNum, parseInt addedNum then divide addedNum by 2
+    // if 0, add 0 to finalBinaryNum, divide addedNum by 2
+    // keep going until parseInt = 0? test this use https://byjus.com/maths/binary-number-system/ to help
     return finalBinaryNum.join('');
 }
