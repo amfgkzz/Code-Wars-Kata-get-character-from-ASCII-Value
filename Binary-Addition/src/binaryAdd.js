ddf4f0486efd9d24761449b065a5c9a08d2b7544
Koua Xiong <kxiong517@gmail.com>
2022-05-04 11:48:33 -0500

    final

--- Binary-Addition/src/binaryAdd.js
+++ Binary-Addition/src/binaryAdd.js
@@ -2,11 +2,10 @@ module.exports = function binaryAdd(num1, num2){
     let addedNum = num1 + num2;
     let finalBinaryNum = [];
 
-    while (addedNum >= 1) {
+    while (addedNum > 0) {
         if (addedNum % 2) {
             finalBinaryNum.unshift(1);
             addedNum = parseInt(addedNum / 2);
-            console.log('addedNum: ', addedNum);
         } else {
             finalBinaryNum.unshift(0);
             addedNum /= 2;
