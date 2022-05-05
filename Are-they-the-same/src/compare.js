77c8e8a3c632438ca5d9d093c10501a19f353b73
Koua Xiong <kxiong517@gmail.com>
2022-05-05 18:38:34 -0500

    work on sort function

--- Are-they-the-same/src/compare.js
+++ Are-they-the-same/src/compare.js
@@ -3,5 +3,20 @@ function compare(arr1, arr2) {
     arr1.sort(function(a, b){return a-b});
     arr2.sort(function(a, b){return a-b});
 
-    
+    for (let i = 0; i < arr1.length; i++) {
+        const elementOne = arr1[i];
+        const elementTwo = arr2[i];
+        
+        if (Math.sqrt(elementTwo) !== elementOne) {
+            return false;
+        }
+    }
+}
+
+function check(a, b){
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return false;
+    } else {
+        return a-b;
+    }
 }
