ea0ce577b72381bcad72b4a29726537ec1731747
Koua Xiong <kxiong517@gmail.com>
2022-06-10 01:24:19 -0500

    compare

--- Are-they-the-same/src/compare.js
+++ Are-they-the-same/src/compare.js
@@ -1,20 +1,6 @@
 function compare(arr1, arr2) {
     let check = false;
 
-    for (let i = 0; i < arr1.length; i++) {
-        const element = arr1[i];
-        if (typeof element !== 'number' ) {
-            return;
-        }
-    }
-
-    for (let i = 0; i < arr2.length; i++) {
-        const element = arr2[i];
-        if (typeof element !== 'number' ) {
-            return;
-        }
-    }
-
     arr1.sort(function(a, b){return a-b});
     arr2.sort(function(a, b){return a-b});
 
